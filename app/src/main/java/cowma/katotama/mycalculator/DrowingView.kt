13ab240521bd9641df7f749ad5e471ac2c9aab3a
Takuma Kato <k.t.mifune@gmail.com>
2020-06-04 23:46:01 +0900

    no message

---
+++ app/src/main/java/cowma/katotama/mycalculator/DrowingView.kt
@@ -0,0 +1,10 @@
+package cowma.katotama.mycalculator
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+
+class DrowingView(context : Context, attr : AttributeSet) : View(context, attr) {
+
+    private val mDrawPath : CustomPath? = null
+}
