14044e27bada9c77d45f469d214526dca01cdffb
Takuma Kato <k.t.mifune@gmail.com>
2020-06-05 23:24:52 +0900

    no message

--- app/src/main/java/cowma/katotama/mycalculator/DrowingView.kt
+++ app/src/main/java/cowma/katotama/mycalculator/DrowingView.kt
@@ -1,10 +1,23 @@
 package cowma.katotama.mycalculator
 
 import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.Color
+import android.graphics.Paint
+import android.graphics.Path
 import android.util.AttributeSet
 import android.view.View
 
-class DrowingView(context : Context, attr : AttributeSet) : View(context, attr) {
+class DrowingView(context: Context, attr: AttributeSet) : View(context, attr) {
 
-    private val mDrawPath : CustomPath? = null
+    private var mDrawPath: CustomPath? = null
+    private var mCanvasBitmap:Bitmap? = null
+    private var mDrawPaint :Paint? = null
+    private var mCanvasPaint: Paint? = null
+    private var mbrushSize: Float = 0.toFloat()
+    private var color = Color.BLACK
+
+    internal inner class CustomPath(var colo: Int, var brushThickness: Float) : Path() {
+
+    }
 }
