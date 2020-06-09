20f0db2dea5da6f86ddcaad2a04d8d08bb499e1b
Takuma Kato <k.t.mifune@gmail.com>
2020-06-10 07:11:49 +0900

    no message

--- app/src/main/java/cowma/katotama/mycalculator/DrowingView.kt
+++ app/src/main/java/cowma/katotama/mycalculator/DrowingView.kt
@@ -1,23 +1,58 @@
 package cowma.katotama.mycalculator
 
 import android.content.Context
-import android.graphics.Bitmap
-import android.graphics.Color
-import android.graphics.Paint
-import android.graphics.Path
+import android.graphics.*
 import android.util.AttributeSet
 import android.view.View
 
 class DrowingView(context: Context, attr: AttributeSet) : View(context, attr) {
 
     private var mDrawPath: CustomPath? = null
-    private var mCanvasBitmap:Bitmap? = null
-    private var mDrawPaint :Paint? = null
+    private var mCanvasBitmap: Bitmap? = null
+    private var mDrawPaint: Paint? = null
     private var mCanvasPaint: Paint? = null
-    private var mbrushSize: Float = 0.toFloat()
+    private var mBrushSize: Float = 0.toFloat()
     private var color = Color.BLACK
+    private var canvas: Canvas? = null
 
-    internal inner class CustomPath(var colo: Int, var brushThickness: Float) : Path() {
+    init {
+        setUpDrawing()
+    }
+
+    private fun setUpDrawing() {
+        mDrawPaint = Paint()
+        mDrawPaint!!.color = color
+        mDrawPaint!!.style = Paint.Style.STROKE
+        mDrawPaint!!.strokeJoin = Paint.Join.ROUND
+        mDrawPaint!!.strokeCap = Paint.Cap.ROUND
+
+        mCanvasPaint = Paint(Paint.DITHER_FLAG)
+
+        mDrawPath = CustomPath(color, mBrushSize)
+
+        mBrushSize = 20.toFloat()
+
+    }
+
+    override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
+        super.onSizeChanged(w, h, oldw, oldh)
+
+        mCanvasBitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888)
+        canvas = Canvas(mCanvasBitmap!!)
+    }
+
+    override fun onDraw(canvas: Canvas?) {
+        super.onDraw(canvas)
+        canvas?.drawBitmap(mCanvasBitmap!!, 0f, 0f, mCanvasPaint)
+        if (!mDrawPath!!.isEmpty) {
+            mDrawPaint!!.strokeWidth = mDrawPath!!.brushThickness
+            mDrawPaint!!.color = mDrawPath!!.color
+            canvas?.drawPath(mDrawPath!!, mDrawPaint!!)
+        }
+    }
+
+
+    internal inner class CustomPath(var color: Int, var brushThickness: Float) : Path() {
 
     }
 }
